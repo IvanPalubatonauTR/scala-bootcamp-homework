029e6ee88370372a270918c94eb10bab283d8f93
Ivan_Palubatonau <Ivan.Palubatonau@thomsonreuters.com>
2021-02-01 19:19:53 +0300

    Homework №1 fix add lcm(0,0) case handling

--- src/main/scala/com/evolutiongaming/basics/Basics.scala
+++ src/main/scala/com/evolutiongaming/basics/Basics.scala
@@ -4,5 +4,9 @@ object Basics {
   @scala.annotation.tailrec
   def gcd(a: Int, b: Int): Int = if (b == 0) Math.abs(a) else gcd(b, a % b)
 
-  def lcm(a: Int, b: Int): Int = Math.abs(a * b) / gcd(a, b)
+  def lcm(a: Int, b: Int): Option[Int] =
+    (a, b) match {
+      case (0, 0) => None
+      case _ => Some(Math.abs(a * b) / gcd(a, b))
+    }
 }
