c4cd57afa0ec8cc589842e61ee2238535878956a
IvanPalubatonauTR <ivan.palubatonau@protonmail.com>
2021-04-17 19:22:24 +0300

    SharedStateHW

---
+++ src/main/scala/com/evolutiongaming/effects/SharedStateHW.scala
@@ -0,0 +1,81 @@
+package com.evolutiongaming.effects
+
+
+import cats.Monad
+import cats.effect.concurrent.Ref
+import cats.effect.{Clock, Concurrent, ExitCode, IO, IOApp, Timer}
+import cats.implicits._
+
+import scala.concurrent.duration._
+
+object SharedStateHW extends IOApp {
+
+  trait Cache[F[_], K, V] {
+    def get(key: K): F[Option[V]]
+
+    def put(key: K, value: V): F[Unit]
+  }
+
+  class RefCache[F[_] : Clock : Monad, K, V](
+                                              state: Ref[F, Map[K, (Long, V)]],
+                                              expiresIn: FiniteDuration
+                                            ) extends Cache[F, K, V] {
+
+    def get(key: K): F[Option[V]] = for {
+      map <- state.get
+      value <- map.get(key).traverse { case (_, value) => Monad[F].pure(value) }
+    } yield value
+
+    def put(key: K, value: V): F[Unit] = for {
+      time <- Clock[F].realTime(MILLISECONDS)
+      _ <- state.update(_ + (key -> (time + expiresIn.toMillis, value)))
+    } yield ()
+
+  }
+
+  object Cache {
+    def of[F[_] : Clock, K, V](
+                                expiresIn: FiniteDuration,
+                                checkOnExpirationsEvery: FiniteDuration
+                              )(implicit T: Timer[F], C: Concurrent[F]): F[Cache[F, K, V]] = for {
+      state <- Ref.of[F, Map[K, (Long, V)]](Map.empty)
+      _ <- T.sleep(checkOnExpirationsEvery)
+      now <- Clock[F].realTime(MILLISECONDS)
+      _ <- state.update { map => map.filter { case (_, (expiredAt, _)) => expiredAt > now } }
+      cache <- new RefCache(state, expiresIn).pure[F]
+    } yield cache
+
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+
+    for {
+      cache <- Cache.of[IO, Int, String](10.seconds, 4.seconds)
+      _ <- cache.put(1, "Hello")
+      _ <- cache.put(2, "World")
+      _ <- cache.get(1).flatMap(s => IO {
+        println(s"first key $s")
+      })
+      _ <- cache.get(2).flatMap(s => IO {
+        println(s"second key $s")
+      })
+      _ <- IO.sleep(12.seconds)
+      _ <- cache.get(1).flatMap(s => IO {
+        println(s"first key $s")
+      })
+      _ <- cache.get(2).flatMap(s => IO {
+        println(s"second key $s")
+      })
+      _ <- IO.sleep(12.seconds)
+      _ <- cache.get(1).flatMap(s => IO {
+        println(s"first key $s")
+      })
+      _ <- cache.get(2).flatMap(s => IO {
+        println(s"second key $s")
+      })
+    } yield ExitCode.Success
+  }
+}
+
+
+
